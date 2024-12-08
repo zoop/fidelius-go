1809b8e538ad009dd5d5b1f2632090f772cca60d
Deepak <deepak@zoop.one>
2024-12-09 00:42:24 +0530

    updates

--- keypairgen/generate.go
+++ keypairgen/generate.go
@@ -1,13 +1,11 @@
 package keypairgen
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/rand"
-	"crypto/x509"
 	"encoding/base64"
 
 	"github.com/pkg/errors"
+	"golang.org/x/crypto/curve25519"
 )
 
 /* -------------------------------------------------------------------------- */
@@ -22,9 +20,9 @@ func (k *keyPairGenHandler) Generate() (*KeyMaterial, error) {
 		return keyMaterial, errors.Wrap(err, "[generateKeyPair][Generate]")
 	}
 
-	privateKeyBase64 := base64.StdEncoding.EncodeToString(privateKey.D.Bytes())
+	privateKeyBase64 := base64.StdEncoding.EncodeToString(privateKey)
 
-	publicKeyBase64 := k.encodePublicKeyToBase64(publicKey)
+	publicKeyBase64 := base64.StdEncoding.EncodeToString(publicKey)
 	x509PublicKeyBase64, err := k.encodeX509PublicKeyToBase64(publicKey)
 	if err != nil {
 		return keyMaterial, errors.Wrap(err, "[encodeX509PublicKeyToBase64][Generate]")
@@ -46,28 +44,30 @@ func (k *keyPairGenHandler) Generate() (*KeyMaterial, error) {
 /* -------------------------------------------------------------------------- */
 /*                               GenerateKeyPair                              */
 /* -------------------------------------------------------------------------- */
-func (k *keyPairGenHandler) generateKeyPair() (*ecdsa.PrivateKey, *ecdsa.PublicKey, error) {
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+func (k *keyPairGenHandler) generateKeyPair() ([]byte, []byte, error) {
+	// Generate a random 32-byte private key for Curve25519
+	privateKey := make([]byte, 32)
+	_, err := rand.Read(privateKey)
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "[ecdsa.GenerateKey][generateKeyPair]")
+		return nil, nil, errors.Wrap(err, "[rand.Read][generateKeyPair]")
 	}
-	return privateKey, &privateKey.PublicKey, nil
-}
 
-func (k *keyPairGenHandler) encodePublicKeyToBase64(publicKey *ecdsa.PublicKey) string {
-	publicKeyBytes := elliptic.Marshal(publicKey.Curve, publicKey.X, publicKey.Y)
-	return base64.StdEncoding.EncodeToString(publicKeyBytes)
+	// Generate the public key from the private key
+	publicKey, err := curve25519.X25519(privateKey, curve25519.Basepoint)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "[curve25519.X25519][generateKeyPair]")
+	}
+
+	return privateKey, publicKey, nil
 }
 
 /* -------------------------------------------------------------------------- */
 /*                         EncodeX509PublicKeyToBase64                        */
 /* -------------------------------------------------------------------------- */
-func (k *keyPairGenHandler) encodeX509PublicKeyToBase64(publicKey *ecdsa.PublicKey) (string, error) {
-	x509Encoded, err := x509.MarshalPKIXPublicKey(publicKey)
-	if err != nil {
-		return "", errors.Wrap(err, "[x509.MarshalPKIXPublicKey][encodeX509PublicKeyToBase64]")
-	}
-	return base64.StdEncoding.EncodeToString(x509Encoded), nil
+func (k *keyPairGenHandler) encodeX509PublicKeyToBase64(publicKey []byte) (string, error) {
+	// Since Curve25519 doesn't use traditional X.509 encoding for public keys,
+	// we will use the raw public key for encoding.
+	return base64.StdEncoding.EncodeToString(publicKey), nil
 }
 
 /* -------------------------------------------------------------------------- */
